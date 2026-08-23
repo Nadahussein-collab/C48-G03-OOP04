b13bdc0cc85d513c238d7c352979d212608f5966
Nadahussein-collab <husseinnada66@gmail.com>
2026-08-23 16:03:13 +0300

    Practical

---
+++ se4.OOP/DeliverHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace se4.OOP
+{
+    public static class DeliveryHelper
+
+    {
+
+        public static void PrintShipmentDetails(Shipment shipment)
+
+        {
+
+            if (shipment != null)
+
+            {
+
+                shipment.PrintShipment();
+
+            }
+
+        }
+
+    }
+}
--- se4.OOP/Program.cs
+++ se4.OOP/Program.cs
@@ -29,7 +29,36 @@
              * Multiple Interfaces: Yes, a class can implement multiple interfaces at the same time.        
              */
             #endregion
-
+            #region Practical
+            //DeliveryAdress address = new DeliveryAdress("Cairo", "El-Tahrir St", 15);
+            //StandardShipment standard = new StandardShipment("SH001", "Books Package", 2.5m, 40m, address);
+            //ExpressShipment express = new ExpressShipment("SH002", "Urgent Documents", 1.0m, 60m, address, 20m);
+            //InternationalShipment international = new InternationalShipment("SH003", "Electronics", 5.0m, 100m, address, "UAE", 50m);
+            //DeliveryCenter center = new DeliveryCenter("Main Cairo Center");
+            //center.AddShipment(standard);
+            //center.AddShipment(express);
+            //center.AddShipment(international);
+            //Console.WriteLine("--- E. All Shipment Details ---");
+            //center.PrintAllShipments();
+            //Console.WriteLine("\n--- F. Tracking Status of Every Shipment ---");
+            //center.PrintTrackingStatuses();
+            //Console.WriteLine("\n--- G. Insurance Cost of Every Shipment ---");
+            //DeliveryReport.PrintInsurance(standard);
+            //DeliveryReport.PrintInsurance(express);
+            //DeliveryReport.PrintInsurance(international);
+            //Console.WriteLine("\n--- H. ITrackable[] Polymorphism ---");
+            //ITrackable[] trackableItems = new ITrackable[] { standard, express, international };
+            //foreach (ITrackable t in trackableItems)
+            //{
+            //    DeliveryReport.PrintShipment(t);
+            //}
+            //Console.WriteLine("\n--- I. IInsurable[] Polymorphism ---");
+            //IInsurable[] insurableItems = new IInsurable[] { standard, express, international };
+            //foreach (IInsurable i in insurableItems)
+            //{
+            //    DeliveryReport.PrintInsurance(i);
+            //}
+            #endregion
         }
     }
 }
